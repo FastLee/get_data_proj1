dfd7de84018181a6e05feb1b805440a35943c119
FastLee <lbareket@gmail.com>
2015-03-21 12:24:53 -0400

    Adding details to the Dictionary

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(LaF)
 ## Getting the list of features (Variables)
-features<-read.csv("../r/UCI HAR Dataset/features.txt",sep=" ", header=FALSE)
+features<-read.csv("./features.txt",sep=" ", header=FALSE)
 ## Subsetting the list of measure to extract the standard diviations and means
 std_mean_features<-features[c(grep("mean",features$V2),grep("std",features$V2)),]
 names(std_mean_features)<-c("col_no","col_name")
@@ -10,27 +10,27 @@ std_mean_features$col_name<-gsub("-","_",gsub("\\(\\)","",std_mean_features$col_
 ## We will first read them using the LaF package.
 ## We will pick only the column specified in std_mean_features
 ## Test Data
-x_test_fwf<-laf_open_fwf("../r/UCI HAR Dataset/test/X_test.txt",rep("numeric",561), rep(16,561))
+x_test_fwf<-laf_open_fwf("./test/X_test.txt",rep("numeric",561), rep(16,561))
 x_test<-x_test_fwf[,std_mean_features$col_no]
 names(x_test)<-std_mean_features$col_name
-y_test_file<-laf_open_csv("../r/UCI HAR Dataset/test/subject_test.txt","numeric","Activity")
+y_test_file<-laf_open_csv("./test/subject_test.txt","numeric","Activity")
 y_test<-y_test_file[,1]
-sub_test_file<-laf_open_csv("../r/UCI HAR Dataset/test/subject_test.txt","numeric","Subject")
+sub_test_file<-laf_open_csv("./test/subject_test.txt","numeric","Subject")
 sub_test<-sub_test_file[,1]
 ## Train Data
-x_train_fwf<-laf_open_fwf("../r/UCI HAR Dataset/train/x_train.txt",rep("numeric",561), rep(16,561))
+x_train_fwf<-laf_open_fwf("./train/x_train.txt",rep("numeric",561), rep(16,561))
 x_train<-x_train_fwf[,std_mean_features$col_no]
 names(x_train)<-std_mean_features$col_name
-y_train_file<-laf_open_csv("../r/UCI HAR Dataset/train/Y_train.txt","numeric","Activity")
+y_train_file<-laf_open_csv("./train/Y_train.txt","numeric","Activity")
 y_train<-y_train_file[,1]
-sub_train_file<-laf_open_csv("../r/UCI HAR Dataset/train/subject_train.txt","numeric","Subject")
+sub_train_file<-laf_open_csv("./train/subject_train.txt","numeric","Subject")
 sub_train<-sub_train_file[,1]
 ## Combining the data frames, now that we have the data in 4 data frames we will combine them together.
 test<-cbind(y_test,sub_test,x_test)
 train<-cbind(y_train,sub_train,x_train)
 train_test<-rbind(train,test)
 ## Now we will decode the Activity Names
-activity<-read.csv("../r/UCI HAR Dataset/activity_labels.txt",sep=" ",col.names=c("Activity","Activity_Name"),colClasses=c("numeric","character"), header=FALSE)
+activity<-read.csv("./activity_labels.txt",sep=" ",col.names=c("Activity","Activity_Name"),colClasses=c("numeric","character"), header=FALSE)
 act_train_test<-merge(activity,train_test,by="Activity") %>% dplyr::select(-Activity)
 ## We write out the table for the submission
 # write.table(act_train_test,"activity_subset.txt",row.names=FALSE)
